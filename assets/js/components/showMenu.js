934318411765db44734aecc86ed88475657ae1c0
Kevin Cifuentes <kevmusic25@gmail.com>
2023-05-07 15:45:04 -0500

    Add more products to main and developing some JS interaction

--- assets/js/components/showMenu.js
+++ assets/js/components/showMenu.js
@@ -1,12 +1,20 @@
-function showMenu () {
-    const nav = document.querySelector('.nav')
-    const menu = document.querySelector('.nav-menu')
+function showMenu() {
+  const nav = document.querySelector('.nav')
+  const menu = document.querySelector('.nav-menu')
 
-    nav.addEventListener('click', function (e) {
-        if (e.target.closest('.btn-menu')) {
-            menu.classList.toggle('show-menu')
-        }
-    })
+  nav.addEventListener('click', function (e) {
+    if (e.target.closest('.btn-menu')) {
+      menu.classList.toggle('show-menu')
+    }
+
+    if (e.target.closest('.btn-close')) {
+      menu.classList.remove('show-menu')
+    }
+
+    if (e.target.closest('.nav-link')) {
+        menu.classList.remove('show-menu')
+      }
+  })
 }
 
 export default showMenu
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 import loader from  './components/loader.js'
 import showMenu from './components/showMenu.js' 
+import showCart from './components/showCart.js'
+import products from './components/products.js'
+import getProducts from './helpers/getProducts.js'
+
 
 /* UI Elements */
 /* Ocultar loader */
@@ -9,3 +13,13 @@ loader()
 /* Mostrar menú*/
 
 showMenu() 
+
+/* Mostrar carrito */
+
+showCart()
+
+/* End UI Elements */
+
+/* Products */
+
+products(await getProducts())
